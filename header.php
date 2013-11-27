3519972165517053561a82c61ae0aaf3c7ba8523
michael <shijn.michael@gmail.com>
2013-11-27 22:42:53 +0800

    First commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>
+			<?php
+
+				wp_title( '-', true, 'right' );
+
+				bloginfo( 'name' );
+
+			?>
+		</title>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+
+		<p>This is the header.</p>
+
+		<?php
+
+			$args = array(
+				'menu' => 'main-menu'
+			);
+
+
+			wp_nav_menu( $args );
+
+		?>
